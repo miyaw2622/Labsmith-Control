c35bca7be17515e20c0aa86eb7aa4df614b3e5cf
miyaw2622 <miya.wanna22@imperial.ac.uk>
2025-03-02 16:14:35 +0000

    deleted:    Matlab/example_events.m 	deleted:    Matlab/testing.m 	deleted:    Python/example_events.py 	deleted:    Python/testing.py

--- Matlab/testing.m
+++
@@ -1,3 +0,0 @@
-py.importlib.import_module('uProcess_x64')
-a=py.uProcess_x64.CEIB().InitConnection(int8(3))
-disp(a)
--- Python/testing.py
+++
@@ -1,8 +0,0 @@
-from uProcess_x64 import uProcess_x64
-
-import numpy as np
-
-print(uProcess_x64)
-
-a = uProcess_x64.CEIB().InitConnection(np.int8(4))
-print(a)
